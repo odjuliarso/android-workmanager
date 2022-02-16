a43912037556d818776ceeead833b32dece3f9ea
Othman Djuliarso <37389043+dothmandev@users.noreply.github.com>
2022-02-16 13:43:18 -0800

    adding a Worker class overriding the doWork method

--- app/src/main/java/com/example/background/workers/BlurWorker.java
+++ app/src/main/java/com/example/background/workers/BlurWorker.java
@@ -1,12 +1,21 @@
 package com.example.background.workers;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
+import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.background.R;
+
 public class BlurWorker extends Worker {
+
+    private static final String TAG = BlurWorker.class.getSimpleName();
 
     // Constructor
     public BlurWorker(
@@ -18,8 +27,32 @@ public class BlurWorker extends Worker {
     //Overriding the doWork() method to use WorkerUtil's makeStatusNotification
     @NonNull
     @Override
-    public  doWork() {
-        Context applicationContextgetApplicationContext();
+    public Result doWork() {
+        Context applicationContext = getApplicationContext();
 
+        //Creating a bitmap using a try/catch statement
+        try {
+            //Bitmap file from the test image
+            Bitmap picture = BitmapFactory.decodeResource(
+                    applicationContext.getResources(),
+                    R.drawable.android_cupcake);
+            //Blur the bitmap
+            Bitmap outpuit = WorkerUtils.blurBitmap(picture, applicationContext);
+
+            //Write bitmap to a temp file
+            Uri outputUri = WorkerUtils.writeBitmapToFile(applicationContext, outpuit);
+
+            WorkerUtils.makeStatusNotification("Output is" + outputUri.toString(), applicationContext);
+
+            //If there were no errors, return SUCEESS
+            return Result.success();
+
+        } catch (Throwable throwable) {
+            //Technically WorkManager will return Result.failure()
+            // but it's best to be explicit about it.
+            // Thus if there were error, we return FAILURE
+            Log.e(TAG, "Error applying blur", throwable);
+            return Result.failure();
+        }
     }
 }
