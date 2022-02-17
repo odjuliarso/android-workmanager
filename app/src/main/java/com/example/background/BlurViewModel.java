8cd54c0e60ff9e68467d17e30bf6e5d52358586e
Othman Djuliarso <37389043+dothmandev@users.noreply.github.com>
2022-02-16 16:58:08 -0800

    adding Data input and output object for passing the image's Uri

--- app/src/main/java/com/example/background/BlurViewModel.java
+++ app/src/main/java/com/example/background/BlurViewModel.java
@@ -16,8 +16,11 @@
 
 package com.example.background;
 
+import static com.example.background.Constants.KEY_IMAGE_URI;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
@@ -36,7 +39,7 @@ public class BlurViewModel extends ViewModel {
 
     private Uri mImageUri;
 
-    // BlurViewModel constructor
+    // Constructor
     public BlurViewModel(@NonNull Application application) {
         super();
         mWorkManager = WorkManager.getInstance(application);
@@ -44,12 +47,27 @@ public class BlurViewModel extends ViewModel {
         mImageUri = getImageUri(application.getApplicationContext());
     }
 
+     /** Creates the input data bundle which includes the Uri to operate on
+     * @return Data which contains the Image Uri as a String
+     * */
+     private Data createInputDataForUri() {
+         Data.Builder builder = new Data.Builder();
+         if (mImageUri != null) {
+             builder.putString(KEY_IMAGE_URI, mImageUri.toString());
+         }
+         return builder.build();
+     }
+
+
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
+//    Passing the Data object to WorkRequest
     void applyBlur(int blurLevel) {
-        mWorkManager.enqueue(OneTimeWorkRequest.from(BlurWorker.class));
+        OneTimeWorkRequest blurRequest = new OneTimeWorkRequest.Builder(BlurWorker.class).setInputData(createInputDataForUri()).build();
+
+        mWorkManager.enqueue(blurRequest);
     }
 
     private Uri uriOrNull(String uriString) {
--- app/src/main/java/com/example/background/workers/BlurWorker.java
+++ app/src/main/java/com/example/background/workers/BlurWorker.java
@@ -1,15 +1,21 @@
 package com.example.background.workers;
 
+import static com.example.background.Constants.KEY_IMAGE_URI;
+
+import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.background.Constants;
 import com.example.background.R;
 
 public class BlurWorker extends Worker {
@@ -29,12 +35,21 @@ public class BlurWorker extends Worker {
     public Result doWork() {
         Context applicationContext = getApplicationContext();
 
-        //Creating a bitmap using a try/catch statement
+        //A variable to get the input: the URI passed from the Data object
+        String resourceUri = getInputData().getString(KEY_IMAGE_URI);
+
+        //Creating a bitmap utilizing a try/catch statement
         try {
 
-            Bitmap picture = BitmapFactory.decodeResource(
-                    applicationContext.getResources(),
-                    R.drawable.android_cupcake);
+            if(TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input URI");
+                throw new IllegalArgumentException("Invalid input URI");
+            }
+
+            ContentResolver resolver = applicationContext.getContentResolver();
+//            Create a bitmap
+            Bitmap picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri)));
 
             // Blur the bitmap
             Bitmap output = WorkerUtils.blurBitmap(picture, applicationContext);
@@ -45,8 +60,13 @@ public class BlurWorker extends Worker {
             WorkerUtils.makeStatusNotification("Output is "
                     + outputUri.toString(), applicationContext);
 
+            // Output temporary URI as a Data object, using the same (Constant) Key
+            Data outputData = new Data.Builder()
+                    .putString(KEY_IMAGE_URI, outputUri.toString())
+                    .build();
             // If there were no errors, return SUCCESS
-            return Result.success();
+            return Result.success(outputData);
+
         } catch (Throwable throwable) {
 
             // Technically WorkManager will return Result.failure()
